b72d5eb0c8bea6f6df4905db832d9b30fe0f9d30
Akshay_Ashok <akshayashokdj@gmail.com>
2025-10-09 11:42:15 +0100

    fix(AnimatedBlureBackground): new component is being created called BlurAnimWrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <>
-    <Header />
-    <IntroductionSection/>
+      <Header />
+      <IntroductionSection />
     </>
   )
 }
--- src/Containers/Header.tsx
+++ src/Containers/Header.tsx
@@ -2,32 +2,25 @@ import AnimatedBlurBackground from "../Components/AnimatedBlurBackground"
 import profilePhoto from "../assets/Profile_DP.png"
 import { Icons } from "../Components/Icons"
 import { ImageFramming } from "../Components/ImageFramming"
+import BlurAnimWrapper from "../Components/BlurAnimWrapper"
 
 
 export const Header = () => {
     return (
-        <div className="relative w-full h-[543px] flex flex-col items-center justify-between">
-            <AnimatedBlurBackground
-                fillParent
-                count={5}
-                opacity={1}
-                getShape={(i) => {
-                    if (i === 0) return { sides: 0, size: 120, color: "#000", speedScale: 10 };   // big circle
-                    if (i === 1) return { sides: 5, size: 90, color: "#000", speedScale: 10 };
-                    if (i === 2) return { sides: 3, size: 80, color: "#000", speedscale: 10 };
-                    return { sides: 4, size: 120 }; // others: squares, default colors
-                }}
-            />
-            <div className={"p-5 w-full flex flex-col items-end"}>
-                <Icons selectedIcon="Menu" className="w-6 h-6 cursor-pointer" />
-            </div>
-            <ImageFramming imageURL= {profilePhoto} structureSelection = "profile_photo"/>
-            <div className="p-5 w-full flex flex-row justify-end">
-                <Icons selectedIcon="Instagram" className="w-6 h-6 mr-3 cursor-pointer" />
-                <Icons selectedIcon="LinkedIn" className="w-6 h-6 cursor-pointer" />
-            </div>
 
-        </div>
+        <BlurAnimWrapper colors={["#000", "#000", "#000"]}>
+            <div className="p-5 w-full h-[543px] flex flex-col items-center justify-between">
+                <div className={"w-full flex flex-col items-end"}>
+                    <Icons selectedIcon="Menu" className="w-6 h-6 cursor-pointer" />
+                </div>
+                <ImageFramming imageURL={profilePhoto} structureSelection="profile_photo" />
+                <div className="w-full flex flex-row justify-end">
+                    <Icons selectedIcon="Instagram" className="w-6 h-6 mr-3 cursor-pointer" />
+                    <Icons selectedIcon="LinkedIn" className="w-6 h-6 cursor-pointer" />
+                </div>
+            </div>
+        </BlurAnimWrapper >
+
 
     )
 }
